33ad002b8508d1e53d86ea5db3ec1896cee4823c
mihkpode <mihkpode@greeny.cs.tlu.ee>
2016-09-08 13:10:52 +0300

    PHP info page, PHP test page

---
+++ firstphp.php
@@ -0,0 +1,8 @@
+<?php
+
+	$firstName = "Mihkel";
+	$lastName = "Põder";
+	echo "My name is ", $firstName." ".$lastName, ".</br>";
+	echo "I'm just a random student, don't mind this page.<br>"
+	//Just printing out me name because I'm a selfish prick.
+?>
