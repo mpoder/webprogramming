83c2200bed5df2f14b27c0582797348ef270e998
mihkpode <mihkpode@greeny.cs.tlu.ee>
2016-09-08 13:14:24 +0300

    Added a title

--- firstphp.php
+++ firstphp.php
@@ -2,7 +2,9 @@
 
 	$firstName = "Mihkel";
 	$lastName = "Põder";
+	echo "<h1>Title page</h1>";
 	echo "My name is ", $firstName." ".$lastName, ".</br>";
 	echo "I'm just a random student, don't mind this page.<br>"
 	//Just printing out me name because I'm a selfish prick.
+
 ?>
